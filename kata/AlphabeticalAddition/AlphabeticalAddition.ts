1e0d920567f4522b4720067e5bf2872b730b5eb9
Rubén Zagon <ruben.zagon@gmail.com>
2019-12-30 22:22:23 +0000

    enought internet for today

--- kata/AlphabeticalAddition/AlphabeticalAddition.ts
+++ kata/AlphabeticalAddition/AlphabeticalAddition.ts
@@ -1,8 +1,28 @@
+const lettersAlphabetic = ['a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i' ,'j' ,'k', 'l' , 'm' , 'n' , 'ñ' , 'o' , 'p', 'q', 'r', 's', 't', 'u' , 'v', 'w', 'x', 'y', 'z']
+
 export function addLetters(...letters: string[]) {
-  const lettersLower = letters.map(element => {
-      return element.toLowerCase()
+  
+  if (letters.length === 0 || letters[0] ==="z"){return 'z'}
+  
+  return lettersAlphabetic[letters.length].toLowerCase() 
+}
+
+function convertToNumbers(arrayLetters:string[], letters:string[]) {
+  let newArray:number[] = []
+  let found = []
+
+  arrayLetters.forEach((value:string, index: number) => {
+    found.push(arrayLetters.find((element, index) => {
+      letters[index] == element 
+      return index; //toy por aqui
+    }))
+    newArray.push(index)
   });
   
-  return lettersLower
+  console.log('Entontré esto ==>', found);
   
+  return newArray
 }
+
+
+console.log(convertToNumbers(lettersAlphabetic, ['a','b', 'c']));
--- kata/AlphabeticalAddition/__test__/AlphabeticalAddition.test.ts
+++ kata/AlphabeticalAddition/__test__/AlphabeticalAddition.test.ts
@@ -1,7 +1,17 @@
 import {addLetters} from "../AlphabeticalAddition";
 
 describe('Kata AlphabeticalAddition', () => {
-    test('should be lowercase always the letters', () => {
-        expect(addLetters('a', 'B','c')).toStrictEqual(['a', 'b', 'c']);
+
+    test('If not have any letters the function should return "z"', () => {
+      expect(addLetters()).toStrictEqual('z');
     });
+
+    test('should add the next letter on aphabetic and always in lowerCase', () => {
+      expect(addLetters('a', 'B','c')).toStrictEqual('d');
+    });
+
+    test('Enter uppercase and must work', () => {
+      expect(addLetters('a', 'B')).toStrictEqual('c');
+    });
+
 });
