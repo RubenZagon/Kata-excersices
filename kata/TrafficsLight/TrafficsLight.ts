175e2a039fd4709aa1d039c6ca53566e506967da
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-04 20:57:15 +0000

    Me queda hacer que no se borren los semaforos

--- kata/TrafficsLight/TrafficsLight.ts
+++ kata/TrafficsLight/TrafficsLight.ts
@@ -1,45 +1,47 @@
 export function trafficLights(road: string, n: number): string[] {
   //"C...R............G......"
+  //"C.R..G.........G......"
+  let sim: string[] = []
 
-  for (let cicle = 0; cicle <= n; cicle++) {
+  // Separamos en Array para poder manipular el string
+  let roadToArray: string[] = road.split("");
 
-    const roadToArray: string[] = road.split("");
+  for (let cicle = 0; cicle < n; cicle++) {
 
-    let CarPosition: number = roadToArray.indexOf("C");
-    const RedPosition: number = roadToArray.indexOf("R");
     const AmbarPosition: number = roadToArray.indexOf("O");
+    const RedPosition: number = roadToArray.indexOf("R");
     const GreenPosition: number = roadToArray.indexOf("G");
 
+    let CarPosition: number = roadToArray.indexOf("C");
 
-    if (cicle === 5) {
-      roadToArray[RedPosition] = "G";
+
+    // Avance de coche
+    if (roadToArray[CarPosition + 1] !== "R") {
+      roadToArray[CarPosition + 1] = "C";
+      roadToArray[CarPosition] = ".";
     }
 
-    if (cicle === 6) {
+    // Cambio de luces de tráfico
+    if (cicle % 3 === 0 && cicle !== 0) {
+      roadToArray[RedPosition] = "G";
       roadToArray[GreenPosition] = "O";
     }
 
-    if (cicle === 7) {
+    if (cicle % 1 === 0 && cicle !== 0) {
       roadToArray[AmbarPosition] = "R";
     }
 
+
+    // Volvemos a unir el array
     const roadToString: string = roadToArray.join("");
 
-    console.log('CICLO ====> ', cicle);
-    console.log(roadToString);
-    // console.log(`
-    // Red = ${RedPosition}
-    // Ambar = ${AmbarPosition}
-    // Green = ${GreenPosition}
-    // ` );
+    sim.push(roadToString);
 
   }
 
+  sim.forEach((element, index) => {
+    console.log(element, " //", index);
+  });
+
   return [''];
-}
-
-type TrafficsLightsOptions = "G" | "O" | "R"
-
-function handleTrafficsLight(cicle: number, cicleToChange: number, roadToArray: [], positionLight: number, lightToChange: TrafficsLightsOptions) {
-
 }
