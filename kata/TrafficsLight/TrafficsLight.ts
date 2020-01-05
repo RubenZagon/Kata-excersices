36df885cd4373c38388cabcc4e8d6b8bec5b7547
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-05 02:21:13 +0000

    Intento repintar semaforo verde sin exito

--- kata/TrafficsLight/TrafficsLight.ts
+++ kata/TrafficsLight/TrafficsLight.ts
@@ -6,6 +6,8 @@ export function trafficLights(road: string, n: number): string[] {
   // Separamos en Array para poder manipular el string
   let roadToArray: string[] = road.split("");
 
+  let times = 2;
+
   for (let cicle = 0; cicle < n; cicle++) {
 
     const AmbarPosition: number = roadToArray.indexOf("O");
@@ -14,11 +16,29 @@ export function trafficLights(road: string, n: number): string[] {
 
     let CarPosition: number = roadToArray.indexOf("C");
 
+    if (roadToArray[CarPosition + 1] === "R") {
 
+    }
     // Avance de coche
     if (roadToArray[CarPosition + 1] !== "R") {
       roadToArray[CarPosition + 1] = "C";
       roadToArray[CarPosition] = ".";
+    }
+
+    let stateOnR = false;
+    // que repinte semaforo verde
+    if (roadToArray[CarPosition + 1] === "R") {
+      console.log("ME HE CHOCADO");
+      stateOnR = true;
+    }
+
+    if (stateOnR === true) {
+      if (times === 0) {
+        console.log('ESTO ESTA ACTIVO - times = ', times);
+        roadToArray[CarPosition - 1] = "G";
+        stateOnR = false;
+      }
+      times--
     }
 
     // Cambio de luces de tráfico
