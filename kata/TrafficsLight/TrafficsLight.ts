3238779116022d4acd1bf4b1f157be2d20a3a1e4
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-07 19:40:08 +0000

    Cambio de todo el código, solo queda contemplar que G dure 5 tiempos aunque C pase por encima

--- kata/TrafficsLight/TrafficsLight.ts
+++ kata/TrafficsLight/TrafficsLight.ts
@@ -1,138 +1,126 @@
 export function trafficLights(road: string, n: number): string[] {
-  //"C...R............G......"
-  //"C.R..G.........G......"
+  /**
+   * VARIABLE GLOBALES
+   */
+
   let sim: string[] = [];
 
   // Separamos en Array para poder manipular el string
   let roadToArray: string[] = road.split("");
 
   //Buscamos la posición de los semaforos
-  const foundSemaforos: number[] = [];
+  const foundTrafficLights: number[] = [];
+  let greenPosition;
 
   for (let i = 0; i < roadToArray.length; i++) {
     if (roadToArray[i] === "G" || roadToArray[i] === "R") {
-      foundSemaforos.push(i);
+      foundTrafficLights.push(i);
     }
   }
 
-  console.log("SEMAFOROS ENCONTRADOS = ", foundSemaforos);
-
-  let times: number = 1;
-  let stateOnR: boolean = false;
-
+  // Tiempos de semaforos
+  const initialTimeGR: number = 5;
   const initialTimeO: number = 1;
-  const initialTimeGR = 4;
-  //Duración de los semaforos
-  let timeGreen: number = initialTimeGR;
-  let timeRed: number = initialTimeGR;
-  let timeOrange: number = initialTimeO;
 
+  let timeGreen = initialTimeGR,
+    timeOrange = initialTimeO,
+    timeRed = initialTimeGR;
+  let durationToReprint = 2;
+
+  /**
+   * PROGRAMA
+   */
+
+  // Loop del programa
   for (let cicle = 0; cicle < n; cicle++) {
-    //Buscamos constantemente la posición del coche
-    let CarPosition: number = roadToArray.indexOf("C");
+    // Cambio luz semaforos
+    if (cicle !== 0) {
+      roadToArray.forEach((element, position) => {
+        // Cambio a Red
+        if (element === "O") {
+          timeOrange--;
+          if (timeOrange === 0) {
+            roadToArray[position] = "R";
+            timeOrange = initialTimeO;
+          }
+        }
 
-    // Avance de coche
-    if (roadToArray[CarPosition + 1] !== "R") {
-      roadToArray[CarPosition + 1] = "C";
-      if (stateOnR === false) {
-        roadToArray[CarPosition] = ".";
+        // Cambio a Orange
+        if (element === "G") {
+          timeGreen--;
+          if (timeGreen === 0) {
+            roadToArray[position] = "O";
+            timeGreen = initialTimeGR;
+          }
+        }
+
+        // Cambio a Green
+        if (element === "R") {
+          timeRed--;
+          if (timeRed === 0) {
+            roadToArray[position] = "G";
+            timeRed = initialTimeGR;
+          }
+        }
+      });
+    }
+
+    // Avance del coche
+    const carPosition = roadToArray.lastIndexOf("C");
+    if (roadToArray[carPosition + 1] !== "R" && cicle !== 0) {
+      // Avance por "."
+      if (roadToArray[carPosition + 1] === ".") {
+        roadToArray[carPosition + 1] = "C";
+        roadToArray[carPosition] = ".";
+      }
+      // Se pone en verde "G"
+      if (roadToArray[carPosition + 1] === "G") {
+        roadToArray[carPosition + 1] = "C";
+        roadToArray[carPosition] = ".";
       }
     }
 
-    // que repinte semaforo verde
-    if (
-      roadToArray[CarPosition + 1] === "R" ||
-      roadToArray[CarPosition + 1] === "G"
-    ) {
-      console.log("ME HE CHOCADO en el ciclo = ", cicle);
-      stateOnR = true;
-    }
-
-    if (stateOnR == true && roadToArray[CarPosition + 1] !== "R") {
-      console.log("--times = ", times, " ++ ciclo = ", cicle);
-      roadToArray[CarPosition + 1] = ".";
-      if (times === 0) {
-        console.log(`
-        El coche está en ${CarPosition}
-        El semaforo estaba en ${foundSemaforos[0 || 1]}
-        
-        IMPRIMO SEMAFORO - times = ${times}
-        `);
-        roadToArray[CarPosition + 1] = "C";
-        roadToArray[CarPosition] = "G";
-        roadToArray[CarPosition - 1] = ".";
-        stateOnR = false;
+    if (roadToArray.lastIndexOf("G") === -1) {
+      durationToReprint--;
+      if (durationToReprint === 0) {
+        roadToArray[carPosition] = "G";
       }
-      times--;
     }
+    greenPosition = roadToArray.lastIndexOf("G");
 
-    // Cambio de luces de tráfico
+    //Volvemos a unir el array
+    const roadToString: string = roadToArray.join("");
+
+    /*
     console.log(`
     CICLO ${cicle}
 
-    timeGreen ${timeGreen}
-    timeOrange ${timeOrange}
-    timeRed ${timeRed}
+    Semáforos:
+      Tiempos: 
+        Green ${timeGreen}
+        Orange ${timeOrange}
+        Red ${timeRed}
+      Posicion  [${foundTrafficLights}]
+      posición del verde ${greenPosition}
+
+    Coche:
+    Posicion actual ${carPosition}
+    Siguiente elemento ${roadToArray[carPosition + 1]}
+
+    ESTADO ACTUAL:
+    ${roadToString}
     `);
+    */
 
-    //  Orange
-    if (roadToArray.indexOf("O") !== -1) {
-      timeOrange--;
-      if (timeOrange === 0) {
-        roadToArray.forEach((element, index) => {
-          if (cicle !== 0 && element === "O") {
-            roadToArray[index] = "R";
-          }
-        });
-      }
-    } else {
-      if (cicle !== 0) {
-        timeOrange = initialTimeO;
-      }
-    }
-
-    // Green
-    if (roadToArray.indexOf("G") !== -1) {
-      timeGreen--;
-      if (timeGreen === 0) {
-        roadToArray.forEach((element, index) => {
-          if (cicle !== 0 && element === "G") {
-            roadToArray[index] = "O";
-          }
-        });
-      }
-    } else {
-      if (cicle !== 0) {
-        timeGreen = initialTimeGR;
-      }
-    }
-
-    // Red
-    if (roadToArray.indexOf("R") !== -1) {
-      timeRed--;
-      if (timeRed === 0) {
-        roadToArray.forEach((element, index) => {
-          if (cicle !== 0 && element === "R") {
-            roadToArray[index] = "G";
-            timeGreen = initialTimeGR - 1;
-          }
-        });
-      }
-    } else {
-      if (cicle !== 0) {
-        timeRed = initialTimeGR;
-      }
-    }
-
-    // Volvemos a unir el array
-    const roadToString: string = roadToArray.join("");
     sim.push(roadToString);
   }
 
-  sim.forEach((element, index) => {
-    console.log(element, " //", index);
-    sim.push(element);
-  });
-
   return sim;
 }
+
+/**
+ * - Si la siguiente posición es un ".", entonces "C" avanza una posición en el Array, si no, no avanza.
+ * - Si la siguiente es una "G", sustituyela por una "C"
+ * - Encontrar las posiciones de los semáforos, donde van a estar siempre
+ * - Rojo y Verde deben durar 5 iteraciones en el FOR, luego cambiar. Y Orange 1 tiempo de iteración
+ */
