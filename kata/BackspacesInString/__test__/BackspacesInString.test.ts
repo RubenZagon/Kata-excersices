68bd43fd84d9f37312f48e44aed4ec05b7fdd725
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-12 15:35:15 +0000

    :white_check_mark: Refactorización de los título en los test
    Test del backspace kata

--- kata/BackspacesInString/__test__/BackspacesInString.test.ts
+++ kata/BackspacesInString/__test__/BackspacesInString.test.ts
@@ -2,25 +2,16 @@ import { cleanString } from "../BackspacesInString";
 
 describe("Clean String", () => {
 
-  describe('Lee solo un #', () => {
-    test("Con una letra - Si lee un # debe borrar el # con la anterior letra", () => {
+  describe('Si lee solo un #', () => {
+    test("Debería realizar un solo borrado de la tecla backspace dónde esté posicionado el #", () => {
       expect(cleanString("a#")).toEqual("");
-    });
-
-    test("Con dos letras - Si lee un # debe borrar el # con la anterior letra", () => {
       expect(cleanString("ba#")).toEqual("b");
-    });
-
-    test('Con tres letras y dos #', () => {
       expect(cleanString("cj#b#")).toEqual("c");
-    });
-
-    test('Con cuatro letras y tres #', () => {
       expect(cleanString("cj#b#c#")).toEqual("c");
     });
   });
 
-  describe('Contador ##', () => {
+  describe('Si lee ## seguido o más', () => {
     test('Con dos hashtags seguidos, debe borrar tantas veces como # se encuentre', () => {
       expect(cleanString("cjg##")).toEqual("c");
     });
