fd36dd08adf94d4c4c2930ad3ca615c7a7491b66
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-13 20:00:55 +0000

    :sparkles: FINALIZADO
    Limpieza de código, actualización de test y probado en codewars OK

--- kata/BackspacesInString/BackspacesInString.ts
+++ kata/BackspacesInString/BackspacesInString.ts
@@ -1,51 +1,20 @@
 export function cleanString(s: string) {
-  let stringToArray: string[] = []
-  let foundHashtag: number[] = []
-  let counterOfBackSpaces: number = 0;
+  let stringToArray: string[] = s.split("");
 
-  stringToArray = s.split("");
+  for (let i = 0; i < s.length; i++) {
+    let indexOfFirstHashtag = stringToArray.indexOf("#");
 
-  stringToArray.forEach((element, index) => {
+    stringToArray[indexOfFirstHashtag] = "";
+    stringToArray[indexOfFirstHashtag - 1] = "";
 
-    if (element === "#") {
-      foundHashtag.push(index)
-
-      // Busco dos # seguidos
-      stringToArray[index + 1] === "#" ? counterOfBackSpaces++ : "";
-
-      // Si el anterior al # es una letra, la borro de inmediato
-      if (stringToArray[index - 1] !== "#") {
-        stringToArray[index] = "";
-        stringToArray[index - 1] = "";
+    stringToArray = stringToArray.filter(element => {
+      if (element !== "") {
+        return element
       }
-    }
-  });
-
-
-
-  // -- Buscar la primera posición del hueco vació, y borrar uno pa atras
-  for (let i = 0; i < counterOfBackSpaces; i++) {
-    let findIndexFirstEmpty = stringToArray.indexOf("");
-    stringToArray[findIndexFirstEmpty - 1] = "";
+    })
   }
 
-
-  const result = stringToArray.join("")
-
-  /*
-  console.log(`
-  Counter : ${counterOfBackSpaces}
-  
-  longitud Hastag ${foundHashtag.length}
-  
-  Hastags encontrados [${foundHashtag}]
-  
-  ENTRADA:
-  [${s}]
-  
-  SALIDA:
-  `, stringToArray);
-  */
+  const result: string = stringToArray.join("")
 
   return result
 }
--- kata/BackspacesInString/__test__/BackspacesInString.test.ts
+++ kata/BackspacesInString/__test__/BackspacesInString.test.ts
@@ -22,18 +22,22 @@ describe("Clean String", () => {
       expect(cleanString("abc#d##c")).toEqual("ac");
       expect(cleanString("abc####d##c#")).toEqual("");
       expect(cleanString("abjd####jfk#")).toEqual("jf");
-      expect(cleanString("gfh#jds###d#dsd####dasdaskhj###dhkjs####df##s##d##")).toEqual("gdasda");
     })
   });
 
-  xdescribe('Randoms', () => {
+  test('Una cadena mega larga', () => {
+    expect(cleanString("gfh#jds###d#dsd####dasdaskhj###dhkjs####df##s##d##")).toEqual("gdasda");
+
+  });
+
+  describe('Randoms', () => {
     test('Contemplando mayúsculas y números', () => {
       expect(cleanString("gIOuEVAo8#wyIy#")).toEqual("gIOuEVAowyI");
       expect(cleanString("rG1###wknb####")).toEqual("");
       expect(cleanString("b#m#sqRE#vxc##X")).toEqual("sqRvX");
     });
     test('Con espacios de por medio', () => {
-      expect(cleanString("d####E#pqfg## #")).toEqual("pqf");
+      expect(cleanString("d####E#pqfg## #")).toEqual("pq");
     });
   });
 });
