9f7f89c4bca9929773e19ead3e5a94fcc76d65fc
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-14 12:55:08 +0000

    :construction: Trabajando en verMirror function
    Me cuesta que me escape el backslash con la n

--- kata/movesInSquaredString/__test__/movesInSquaredString.test.ts
+++ kata/movesInSquaredString/__test__/movesInSquaredString.test.ts
@@ -3,21 +3,29 @@ import { vertMirror, horMirror, oper } from '../movesInSquaredString'
 describe("Kata-movesInSquaredString", () => {
 
   describe.only('Función - verMirror', () => {
-    test("Should ", () => {
-      expect(vertMirror("aab")).toEqual()
+    test("Debe cambiar el orden de las palabras", () => {
+      expect(vertMirror("abcd")).toEqual("dcba")
+      expect(vertMirror("efgh")).toEqual("hgfe")
+      expect(vertMirror("ijkl")).toEqual("lkji")
+      expect(vertMirror("mnop")).toEqual("ponm")
     })
+
+    test.only('Debe cambiar el orden de las palabras en bloques', () => {
+      expect(vertMirror("dcba\nhgfe")).toEqual("abcd\nefgh");
+      expect(vertMirror("dcba\nhgfe\nlkji\nponm")).toEqual("abcd\nefgh");
+    });
   });
 
   describe('Función - horMirror', () => {
     test("Should ", () => {
-      expect(horMirror("aab")).toEqual()
+      expect(horMirror("aab")).toEqual("aab")
     })
   });
 
-  xdescribe('Función - oper', () => {
-    test("Should ", () => {
-      expect(oper(vertMirror("aab"), "aab")).toEqual()
-    })
-  });
+  // xdescribe('Función - oper', () => {
+  //   test("Should ", () => {
+  //     expect(oper(vertMirror("aab"), "aab")).toEqual()
+  //   })
+  // });
 
 })
--- kata/movesInSquaredString/movesInSquaredString.ts
+++ kata/movesInSquaredString/movesInSquaredString.ts
@@ -1,7 +1,26 @@
-export function vertMirror(strng: string) {
-  return ""
+export function vertMirror(s: string) {
+
+  let blockOfLetters: string[] = s.split("\n");
+
+  let vertOrderArray: string[] = blockOfLetters.map((el, index) => {
+    let letters: string[] = el.split("");
+    // Reordena los bloques de palabras
+    let vertOrderBlock = letters.reduceRight((acc, element) => {
+      return acc + element
+    })
+
+    return index !== 0 ? "n" + vertOrderBlock : vertOrderBlock;
+  })
+
+  console.log(`
+  Array nuevas letras ${blockOfLetters}
+
+  Reordenada ${vertOrderArray.join("\\\ ")}
+  `);
+
+  return vertOrderArray.join("\\\ ")
 }
-export function horMirror(strng: string) {
+export function horMirror(s: string) {
   return ""
 }
 export function oper(fct: (s: string) => string, s: string) {
